3a474e4760a93bd2c38b6bbe93b4f272581f7f35
Bastien Ho <bastien@avecnous.eu>
2020-04-24 12:56:37 +0200

    Missing useful function

--- jquery-tabs.js
+++ jquery-tabs.js
@@ -5,6 +5,24 @@
  */
 const $ = window.jQuery;
 
+const str_to_slug = function(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to   = "aaaaeeeeiiiioooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+}
+
 const js_tab_openTab = function(selector){
  var Target = $('#'+selector);
  var Wrapper = Target.parents('.front-tabs-content');
